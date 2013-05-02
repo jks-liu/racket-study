c168a7691396e38c636480ea7e463dbba107fc23
jks-liu <chinatianma@gmail.com>
2013-05-02 22:47:18 +0800

    small change

--- sicp3.rkt
+++ sicp3.rkt
@@ -18,3 +18,20 @@
                   ((eq? m 'deposit) deposit)
                   (else (error "Eror balance" m))))))
     dispatch))
+
+(define (monte-carlo trials experiment)
+  (letrec ((iter 
+          (lambda (trials-remaining trials-passed)
+            (cond ((= trials-remaining 0)
+                   (/ trials-passed trials))
+                  (else 
+                   (iter (- trials-remaining 1)
+                         (+ trials-passed
+                            (if (experiment) 1 0))))))))
+    (iter trials 0)))
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+(define (cesaro-test)
+  (= (gcd (random #e1e5) (random #e1e5)) 1))
+(estimate-pi 4e4)
+         
